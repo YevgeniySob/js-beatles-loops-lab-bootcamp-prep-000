3d856981cebd296a4b93df6e23740788cadcdcb4
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-02 21:55:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(arr1, arr2){
+  myArray = []
+  for(var i = 0;i < arr1.length; i++){
+    myArray.push(arr1[i] + " " + arr2[i])
+  }
+  return myArray
+}
