bc825d5391d16ddb032421f87484c5e5f9fead07
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-02 22:04:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(arr1, arr2){
   var myArray = []
   for(var i = 0;i < arr1.length; i++){
-    myArray.push(arr1[i] + " " + arr2[i])
+    myArray.push(arr1[i] + " plays " + arr2[i])
   }
   return myArray
 }
